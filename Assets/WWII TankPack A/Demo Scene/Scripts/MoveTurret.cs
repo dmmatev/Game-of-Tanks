f6ad544b4cb5b94fc0e06c060ee7d4db0a7208dc
BRANIAC <dimmat97@gmail.com>
2015-11-13 08:38:12 +0200

    rotating turret with mouse

--- Assets/WWII TankPack A/Demo Scene/Scripts/MoveTurret.cs
+++ Assets/WWII TankPack A/Demo Scene/Scripts/MoveTurret.cs
@@ -23,36 +23,20 @@ public class MoveTurret : MonoBehaviour {
 		if(Mathf.Abs (newRotation.y) - Mathf.Abs(transform.rotation.y) < 1f){
 			Debug.Log("THEY ARE THE SAME");
 		}
+
+
+
+
 		Debug.Log(newRotation.y);
 		Debug.Log(transform.rotation.eulerAngles.y);
-		if(Mathf.Abs (newRotation.y) - Mathf.Abs(transform.rotation.eulerAngles.y) > 1f){
-			//transform.Rotate(0f, Mathf.Lerp(transform.rotation.y,newRotation.y,0.3f), 0f);
-			if(newRotation.y > transform.rotation.eulerAngles.y){
-				transform.Rotate(0f, speed , 0f);
-			}else if(newRotation.y < transform.rotation.eulerAngles.y){
+		if(Mathf.Abs (newRotation.y - transform.rotation.eulerAngles.y) > 1f){
+			float currentRotationY = transform.rotation.eulerAngles.y;
+			if(currentRotationY - newRotation.y < - 180 || currentRotationY - newRotation.y < 180 && currentRotationY - newRotation.y >= 0){
 				transform.Rotate(0f, -speed , 0f);
+			}else{
+				transform.Rotate(0f, speed , 0f);
 			}
-			//transform.Rotate(0f, newRotation.y * speed * Time.deltaTime, 0f);
 		}
-
-//		if(Input.GetAxis("Mouse X") > 0f) {
-//			transform.Rotate(0f, speed , 0f);
-//		}
-//		if(Input.GetAxis("Mouse X") < 0f) {
-//			transform.Rotate(0f, -speed , 0f);
-//		}
-		//transform.eulerAngles = new Vector3(transform.eulerAngles.x, newRotation.y, transform.eulerAngles.z);
-
-
-
-		//transform.LookAt(new Vector3(0f, Mathf.Lerp(newDistance.y, transform.position.y, speed * Time.deltaTime)));
-
-		//transform.Rotate(newDistance );
-
-
-		// Turn Left
-//		if (Input.GetKey (KeyCode.A)) 
-//			transform.Rotate(new Vector3(0, -speed * Time.deltaTime, 0));
 		
 	}
 }
