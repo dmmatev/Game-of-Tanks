61993d41cf49fcdf8967206ae36bc716bcfc7dd7
BRANIAC <dimmat97@gmail.com>
2016-02-15 19:10:39 +0200

    added Main Menu Empty animation and Main Open animation as well as Menu Manager script

--- Assets/GameOfTanks/Main Menu/Scripts/Menu.cs
+++ Assets/GameOfTanks/Main Menu/Scripts/Menu.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Menu : MonoBehaviour {
 
 	private Animator animator;
 	private CanvasGroup canvasGroup;
