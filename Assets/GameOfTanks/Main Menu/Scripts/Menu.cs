43bd301864e83ed1b823ca6e78ab5757f8e10de3
BRANIAC <dimmat97@gmail.com>
2016-02-15 16:02:25 +0200

    added script for disabling/enabling interaction with the menu

---
+++ Assets/GameOfTanks/Main Menu/Scripts/Menu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	private Animator animator;
+	private CanvasGroup canvasGroup;
+
+	public bool IsOpen{
+
+		get {return animator.GetBool("IsOpen"); }
+		set {animator.SetBool("IsOpen",value); }
+	}
+
+	void Awake(){
+
+		animator = GetComponent<Animator>();
+		canvasGroup = GetComponent<CanvasGroup>();
+
+		RectTransform rect = GetComponent<RectTransform>();
+		rect.offsetMax = rect.offsetMin = new Vector2(0,0);
+
+	}
+
+	void Update(){
+		if(!animator.GetCurrentAnimatorStateInfo(0).IsName("Open")){
+			canvasGroup.blocksRaycasts = canvasGroup.interactable = false;
+		}else{
+			canvasGroup.blocksRaycasts = canvasGroup.interactable = true;
+		}
+	}
+
+
+}
