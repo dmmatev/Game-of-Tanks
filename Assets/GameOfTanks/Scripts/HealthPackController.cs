c0c5a437d8a99a5b33e648e66b8773de2fe5b476
BRANIAC <dimmat97@gmail.com>
2016-06-02 05:24:30 +0300

    added light and sound effects to health pack when player collides with it

--- Assets/GameOfTanks/Scripts/HealthPackController.cs
+++ Assets/GameOfTanks/Scripts/HealthPackController.cs
@@ -4,18 +4,25 @@ using System.Collections;
 public class HealthPackController : MonoBehaviour {
 
 	public int addAmountOfHealth = 500;
+	public AudioClip sound;
+	AudioSource audio;
 
 	TankHealth playerHealth;
 
 	void Awake () {
-		
+		audio = GetComponent<AudioSource>();
 	}
 
 	void OnCollisionEnter(Collision collision) {
 		if(collision.gameObject.tag == "Allies"){
 			playerHealth = collision.collider.GetComponent<TankHealth>();
 			playerHealth.addHealth(addAmountOfHealth);
-			Destroy(gameObject);
+
+			audio.PlayOneShot(sound);
+			GetComponent<BoxCollider>().isTrigger = true;
+			GetComponentInChildren<Light>().color = new Color(127,255,0);
+			GetComponentInChildren<Light>().intensity = 0.1f;
+			Destroy(gameObject, sound.length); 
 		}
 	}
 
